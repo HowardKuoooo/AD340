3386fc4000d8ad284704d76c27105e31702a27b4
HowardKuoooo <iamhowardkuo@gmail.com>
2019-05-08 18:26:33 -0700

    Homework 3 and 4

---
+++ HW3/app/src/main/java/com/example/hw3/RecyleView.java
@@ -0,0 +1,13 @@
+package com.example.hw3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecyleView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyle_view);
+    }
+}
