19b81543e917123a49b3c6e0db9cbf191b79b163
HowardKuoooo <iamhowardkuo@gmail.com>
2019-05-14 18:41:52 -0700

    HW5

--- HW5/app/src/main/java/com/example/hw5/TempConversion.java
+++ HW5/app/src/main/java/com/example/hw5/TempConversion.java
@@ -1,4 +1,56 @@
 package com.example.hw5;
 
+
+import android.content.Context;
+
 public class TempConversion {
+
+    //private String name;
+    private double celsius;
+    private double fahrenheit;
+
+
+    public TempConversion(double celsius, double fahrenheit) {
+        this.celsius = celsius;
+        this.fahrenheit = fahrenheit;
+    }
+
+    public double getCelsius(int i) {
+        return celsius;
+    }
+
+    public void setCelsius(double celsius) {
+        this.celsius = celsius;
+    }
+
+    public double getFahrenheit(int i) {
+        return fahrenheit;
+    }
+
+    public void setFahrenheit(double fahrenheit) {
+        this.fahrenheit = fahrenheit;
+    }
+
+    public static double cTof(double celsius){
+        return (celsius*(9/5)) + 32;
+    }
+
+    public static double fToc(double fahrenheit){
+        return (fahrenheit-32)*(5/9);
+    }
+
+    /*
+    pulic TempConversion(Context ctx){
+    this.name = "Default profile"
+
+    String defaulttemp = "32";
+
+    //not sure about the baseRate
+    this.baseRate = Double
+
+
+
+    }
+     */
+
 }
--- HW5/app/src/test/java/com/example/hw5/TempConversionTest.java
+++ HW5/app/src/test/java/com/example/hw5/TempConversionTest.java
@@ -1,4 +1,43 @@
 package com.example.hw5;
 
+import com.google.common.truth.Truth;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.mockito.Mock;
+
+import static com.google.common.truth.Truth.assertThat;
+
 public class TempConversionTest {
+
+    TempConversion tempConversion;
+
+    @Before
+    public void setUp(){
+        this.tempConversion = new TempConversion(0,32);
+    }
+
+    @Test
+    public void TempConversion_ReturnsCorrectValues(){
+        double newCelisius = (double) tempConversion.getCelsius (0);
+        assertThat(newCelisius).isEqualTo (0.0);
+
+        double newFahrenheit = (double) tempConversion.getFahrenheit (32);
+        assertThat(newFahrenheit).isEqualTo (32.0);
+
+        double testctof = (double) tempConversion.cTof (0);
+        assertThat(testctof).isEqualTo (32.0);
+
+        double testftoc = (double) tempConversion.fToc (32);
+        assertThat(testftoc).isEqualTo (0.0);
+
+    }
+
+    @After
+    public void after(){
+
+    }
+
+
 }
